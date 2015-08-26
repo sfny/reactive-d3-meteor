9ed33c3386dbe3d8abdafd2c57f6d7f1cb515cd8
Jacob <sunrisefade+github@gmail.com>
2015-08-25 21:53:07 -0700

    removed silly comments

--- reactive-d3-meteor.js
+++ reactive-d3-meteor.js
@@ -7,25 +7,14 @@ if (Meteor.isServer) {
     }
   });
 
-// If meteor is the server, on startup look for Circle objects
-// if none can be found, inster these 6 values into the Collection (mongo)
-
   Meteor.setInterval(function () {
     var newData = _.shuffle(Circles.findOne().data);
     Circles.update({}, {data: newData});
   }, 2000);
 }
-/* Set interval of 2000ms. Each interval, use Underscore's shuffle method to return a rearangeed COPY of the list of values.
-findOne looks for the first data in a collection that meets the criteria, in this case the value array. 
-Maybe findOne is used to stop the function from looking for more values? Research that later...
 
-The collection.update method takes a min of 2 arguments. The Selector is empty, becasue we're selecting the entire collection (maybe?). 
-The second update argument is 'how to modify the collection'. We want to take the existing 'data' and replace it with 'new Data'.  
-
-*/
-
-if (Meteor.isClient) {                            // OKAY getting to the good stuff.
-  Template.vis.rendered = function () {              // Template.vis.rendered is what we want. But it's only avalible after the funciton is complete.
+if (Meteor.isClient) {                            
+  Template.vis.rendered = function () {
     var svg, width = $(window).width(), height = $(window).height()/6, x;
 
     svg = d3.select('#circles').append('svg')
@@ -42,13 +31,12 @@ if (Meteor.isClient) {                            // OKAY getting to the good st
           .attr('cx', function (d, i) { return x(i); })
           .attr('cy', height / 2);
       } else {
-        circles = circles.transition().duration(2000); // animatate transition 
+        circles = circles.transition().duration(2000);
       }
       circles.attr('r', function (d) { return d*2; });
       circles.attr("fill", function() { return "hsl(" + Math.random() * 360 + ", 100%, 75%)" });
 
     };
-
 
     Circles.find().observe({
       added: function () {
